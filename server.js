398bea9cac5b8e39e27932086993b0ed9e79ded6
JAGeorge54 <jesse.george54@gmail.com>
2024-08-21 14:05:05 -0400

    set up basic express server

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-const connect = require("./connect")
+const connect = require("./connect");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config({path: "./config.env"});
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+    connect.connectToServer()
+    console.log(`Server is running on port: ${process.env.PORT}`)
+})
