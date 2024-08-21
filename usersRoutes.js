49df6ff7e88b1a1e7a9ac2cc3e2abbe87a4ff673
JAGeorge54 <jesse.george54@gmail.com>
2024-08-21 16:58:06 -0400

    added route for find one by id

--- usersRoutes.js
+++ usersRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const database = require("./connect");
+const ObjectId = require("mongodb").ObjectId;
 
 let usersRoutes = express.Router();
 
@@ -16,6 +17,15 @@ usersRoutes.route("/users").get(async (req, res) => {
 })
 
 //#2 Retrieve One
+usersRoutes.route("/users/:id").get(async (req, res) => {
+    let db = database.getDb();
+    let data = await db.collection("users").findOne({_id: new ObjectId(req.params.id)});
+    if (Object.keys(data).length > 0) {
+        res.json(data);
+    } else {
+        throw new Error("Data was not found :(")
+    }
+})
 //#3 Create One
 //#4 Update One
 //#5 Delete One
