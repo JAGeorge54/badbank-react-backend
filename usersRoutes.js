6b074c53650c50baacd6d5886006405dd6b1a0ea
JAGeorge54 <jesse.george54@gmail.com>
2024-08-23 15:04:03 -0400

    created verifyToken function and added to api's

--- usersRoutes.js
+++ usersRoutes.js
@@ -2,13 +2,15 @@ const express = require("express");
 const database = require("./connect");
 const ObjectId = require("mongodb").ObjectId;
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
+require("dotenv").config({path: "./config.env"});
 
 let usersRoutes = express.Router();
 const SALT_ROUNDS = 6;
 
 // #1 Retrieve All
 // http://localhost:3000/users
-usersRoutes.route("/users").get(async (req, res) => {
+usersRoutes.route("/users").get(verifyToken, async (req, res) => {
     let db = database.getDb();
     let data = await db.collection("users").find({}).toArray();
     if (data.length > 0) {
@@ -19,7 +21,7 @@ usersRoutes.route("/users").get(async (req, res) => {
 })
 
 //#2 Retrieve One
-usersRoutes.route("/users/:id").get(async (req, res) => {
+usersRoutes.route("/users/:id").get(verifyToken, async (req, res) => {
     let db = database.getDb();
     let data = await db.collection("users").findOne({_id: new ObjectId(req.params.id)});
     if (Object.keys(data).length > 0) {
@@ -30,7 +32,7 @@ usersRoutes.route("/users/:id").get(async (req, res) => {
 })
 
 //#3 Create One
-usersRoutes.route("/users").post(async (req, res) => {
+usersRoutes.route("/users").post(verifyToken, async (req, res) => {
     let db = database.getDb();
 
     const emailTaken = await db.collection("users").findOne({email: req.body.email});
@@ -55,7 +57,7 @@ usersRoutes.route("/users").post(async (req, res) => {
 })
 
 //#4 Update One
-usersRoutes.route("/users/:id").put(async (req, res) => {
+usersRoutes.route("/users/:id").put(verifyToken, async (req, res) => {
     let db = database.getDb();
     let mongoObject = {
         $set: {
@@ -72,7 +74,7 @@ usersRoutes.route("/users/:id").put(async (req, res) => {
 })
 
 //#5 Delete One
-usersRoutes.route("/users/:id").delete(async (req, res) => {
+usersRoutes.route("/users/:id").delete(verifyToken, async (req, res) => {
     let db = database.getDb();
     let data = await db.collection("users").deleteOne({_id: new ObjectId(req.params.id)});
     res.json(data);
@@ -86,7 +88,8 @@ usersRoutes.route("/users/login").post(async (req, res) => {
     if (user) {
         let confirmation = await bcrypt.compare(req.body.password, user.password);
         if (confirmation) {
-            res.json({success: true, user})
+            const token = jwt.sign(user, process.env.SECRETKEY, {expiresIn: '1h'})
+            res.json({success: true, token})
         } else {
             res.json({success:false, message: 'Incorrect Password.'})
         }
@@ -95,5 +98,22 @@ usersRoutes.route("/users/login").post(async (req, res) => {
     }
 
 })
+
+function verifyToken(request, response, next) {
+    const authHeaders = request.headers["authorization"];
+    const token = authHeaders && authHeaders.split(' ')[1];
+    if (!token) {
+        return response.status(401).json({message: "Authentication token is missing."});
+    }
+
+    jwt.verify(token, process.env.SECRETKEY, (error, user) => {
+        if (error) {
+            return response.status(403).json({message: "Invalid Token."})
+        }
+
+        request.body.user = user;
+        next()
+    })
+}
 
 module.exports = usersRoutes;
