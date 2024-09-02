238cf814b518cdbd1419f17818fde83bdb48b699
JAGeorge54 <jesse.george54@gmail.com>
2024-09-02 09:25:20 -0400

    changed port on server

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(users);
 
-app.listen(process.env.PORT, () => {
+app.listen(3000 || process.env.PORT, () => {
     connect.connectToServer()
     console.log(`Server is running on port: ${process.env.PORT}`)
 })
